207aeb884a0e8dc2565f54a40b38ecec2ffa3442
Muhamad Risman <real.risman@gmail.com>
2022-04-19 19:40:10 +0700

    refactor: add component Center to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
+import Center from '../components/Center'
 import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
   return (
     <div className="h-screen overflow-hidden bg-black">
-      <main className="">
+      <main className="flex">
         <Sidebar />
+        <Center />
       </main>
     </div>
   )
