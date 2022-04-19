bba80ec86e95450a320a9ec1447beae3b7a36d7f
Muhamad Risman <real.risman@gmail.com>
2022-04-19 19:39:51 +0700

    style: add some styles for sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,11 +6,20 @@ import {
   HeartIcon,
   RssIcon,
 } from '@heroicons/react/outline'
+import { signOut, useSession } from 'next-auth/react'
 
 const Sidebar = () => {
+  const { data: session, status } = useSession()
+
   return (
-    <div className="border-r border-gray-900 p-5 text-sm text-gray-500">
+    <div className="h-screen overflow-y-scroll border-r border-gray-900 p-5 text-sm text-gray-500 scrollbar-hide">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Log out</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
